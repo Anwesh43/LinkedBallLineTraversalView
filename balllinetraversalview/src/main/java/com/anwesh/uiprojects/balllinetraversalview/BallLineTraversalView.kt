01b0853a76dc24720c7108305f0b53b1966033e9
Anwesh43 <anweshthecool0@gmail.com>
2019-08-10 13:30:37 +0530

    adding constants and libraries

--- balllinetraversalview/src/main/java/com/anwesh/uiprojects/balllinetraversalview/BallLineTraversalView.kt
+++ balllinetraversalview/src/main/java/com/anwesh/uiprojects/balllinetraversalview/BallLineTraversalView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.balllinetraversalview
 /**
  * Created by anweshmishra on 10/08/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4A148C")
+val backColor : Int = Color.parseColor("#BDBDBD")
