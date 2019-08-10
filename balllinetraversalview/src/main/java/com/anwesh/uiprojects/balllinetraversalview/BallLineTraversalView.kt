bb245fd04fe30a91054edcc41e4a8fdcb6bc75b5
Anwesh43 <anweshthecool0@gmail.com>
2019-08-10 15:06:51 +0530

    adding drawBTLNode canvas extension method

--- balllinetraversalview/src/main/java/com/anwesh/uiprojects/balllinetraversalview/BallLineTraversalView.kt
+++ balllinetraversalview/src/main/java/com/anwesh/uiprojects/balllinetraversalview/BallLineTraversalView.kt
@@ -20,6 +20,8 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4A148C")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 1.9f
+val wFactor : Float = 3f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -30,3 +32,31 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawBall(x : Float, r : Float, scale : Float, paint : Paint) {
+    drawCircle(x, 0f, r * scale, paint)
+}
+
+fun Canvas.drawBallLineTraversal(w : Float, size : Float, scale : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val sc21 : Float = sc2.divideScale(0, 2)
+    val sc22 : Float = sc2.divideScale(1, 2)
+    val x1 : Float = w * sc21
+    val x2 : Float = w * sc22
+    drawBall(w * sc21, r , sc1, paint)
+    drawLine(x2, 0f, x1, 0f, paint)
+}
+
+fun Canvas.drawBLTNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val offsetW : Float = w / (wFactor * nodes)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBallLineTraversal(w + offsetW * (i + 1), size, scale, paint)
+}
